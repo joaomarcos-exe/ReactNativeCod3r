169f2f93628b859d755a11701ae43f3b2f1d9214
João Marcos Pimenta Martins <joaomarcos-icm@hotmail.com>
2022-03-01 08:33:25 -0300

    Flag

--- Minefield/App.js
+++ Minefield/App.js
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet} from 'react-native'
 
 //arquivos criados
 import params from './src/params';
+import Field from './src/components/Field';
 
 export default () =>{
   return(
@@ -11,6 +12,20 @@ export default () =>{
         Started MineField
       </Text>
       <Text>Size: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+
+      <Field/>
+      <Field opened/>
+      <Field opened nearMines={1}/>
+      <Field opened nearMines={2}/>
+      <Field opened nearMines={3}/>
+      <Field opened nearMines={6}/>
+      <Field mined/>
+      <Field mined opened/>
+      <Field mined opened exploded/>
+      <Field flagged/>
+      <Field flagged opened/>
+
+      
     </View>
   )
 }
@@ -20,6 +35,6 @@ const style = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F0F0F0'
-  }
+    backgroundColor: 'grey',
+  },
 })
